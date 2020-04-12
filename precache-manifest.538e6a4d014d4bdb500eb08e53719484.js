60fdc5f30dc366a87d6c9cc1087e4478a5f045ba
Wardun <wardunislamnoon@gmail.com>
2020-04-12 21:20:54 +0600

    Updates

---
+++ precache-manifest.538e6a4d014d4bdb500eb08e53719484.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deb11f2d02e9050c673478a3a00fbe42",
+    "url": "/robofriend/index.html"
+  },
+  {
+    "revision": "d72ab921e8dc38eb6b29",
+    "url": "/robofriend/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "5361b3aff882c1ecd98e",
+    "url": "/robofriend/static/css/main.51c8fa83.chunk.css"
+  },
+  {
+    "revision": "d72ab921e8dc38eb6b29",
+    "url": "/robofriend/static/js/2.60cfdb77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriend/static/js/2.60cfdb77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5361b3aff882c1ecd98e",
+    "url": "/robofriend/static/js/main.fa6de343.chunk.js"
+  },
+  {
+    "revision": "699446d112cc6175bd89",
+    "url": "/robofriend/static/js/runtime-main.9b844223.js"
+  }
+]);
